945926aefb520a416469cbd0f25a32052d66923a
Khôi Nguyễn <khoinpb96@gmail.com>
2024-10-01 23:06:05 +0700

    chore: try another puppeteer config

--- src/services/screenshot.service.ts
+++ src/services/screenshot.service.ts
@@ -6,38 +6,41 @@ export default class ScreeshotService implements ScreenshotServiceABC {
   constructor(private webdriver = puppeteer) {}
 
   public async captureTradingViewChart(symbol: string, interval: string) {
-    console.log("ScreeshotService - start browser");
-    const browser = await this.webdriver.launch();
+    const browser = await this.webdriver.launch({
+      browser: "chrome",
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--disable-gpu",
+        "--no-zygote",
+        "--single-process",
+        "--disable-software-rasterizer",
+      ],
+    });
+    console.log(`Connected: ${browser.connected}`);
 
-    try {
-      console.log(`Connected: ${browser.connected}`);
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 800 });
 
-      const page = await browser.newPage();
-      await page.setViewport({ width: 1200, height: 800 });
+    const url = `https://www.tradingview.com/chart/?symbol=${symbol}&interval=${interval}`;
+    const res = await page.goto(url, { waitUntil: "networkidle0" });
+    await page.waitForSelector(".chart-container", { timeout: 10000 });
 
-      const url = `https://www.tradingview.com/chart/?symbol=${symbol}&interval=${interval}`;
-      const res = await page.goto(url, { waitUntil: "networkidle0" });
-      await page.waitForSelector(".chart-container", { timeout: 10000 });
-
-      if (!res?.ok) {
-        console.error(`ScreeshotService - Cannot load ${symbol} chart`, url);
-        throw new ServiceError(`Cannot load ${symbol} chart`, 500);
-      }
-
-      console.log("ScreeshotService - getting chart");
-      const chart = await page.$(".chart-container");
-      if (!chart) {
-        throw new ServiceError("Cannot select chart-container", 500);
-      }
-
-      console.log("ScreeshotService - screenshoting");
-      return Buffer.from(await chart.screenshot());
-    } catch (error) {
-      console.log("captureTradingViewChart something went wrong", error);
-      throw error;
-    } finally {
-      await browser.close();
-      console.log("Browser closed");
+    if (!res?.ok) {
+      console.error(`ScreeshotService - Cannot load ${symbol} chart`, url);
+      throw new ServiceError(`Cannot load ${symbol} chart`, 500);
     }
+
+    console.log("ScreeshotService - getting chart");
+    const chart = await page.$(".chart-container");
+    if (!chart) {
+      throw new ServiceError("Cannot select chart-container", 500);
+    }
+
+    console.log("ScreeshotService - screenshoting");
+    return Buffer.from(await chart.screenshot());
   }
 }
